ffe8bee06d587e6963c9cb68f9825825a0947492
Bruno Araújo <bruno.ar.avl@gmail.com>
2020-07-10 20:43:14 -0300

    typescript e eslint

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Header from '../components/Header'
+
+type Props = {
+  title: string
+}
+
+export default function Home({ title = 'Teste' } : Props) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <Header></Header>
+    </div>
+  )
+}
