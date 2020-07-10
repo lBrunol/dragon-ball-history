94a37e70895fdecf1142625f0078422ad0416451
Bruno Araújo <bruno.ar.avl@gmail.com>
2020-07-10 01:22:42 -0300

    replace da estrutura básica

---
+++ components/Header.jsx
@@ -0,0 +1,14 @@
+
+export default function Header(){
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#">História</a></li>
+          <li><a href="#">Personagens</a></li>
+          <li><a href="#">Curiosidades</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
